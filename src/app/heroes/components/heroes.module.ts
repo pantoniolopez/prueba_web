56e4d74af9cc48085dc58842562c96c848250a42
Antonio López <pantoniolopez@iesplayamar.es>
2023-11-21 19:12:39 +0100

    Lista arreglada

--- src/app/heroes/components/heroes.module.ts
+++ src/app/heroes/components/heroes.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HeroeComponent } from './heroe/heroe.component';
-import { ListaComponent } from './lista/lista.component';
 import { CommonModule } from '@angular/common';
+import { ListaComponent } from './lista/lista.component';
 
 @NgModule({
   declarations: [HeroeComponent, ListaComponent],
